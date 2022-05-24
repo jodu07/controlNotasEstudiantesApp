414c042662c012db8899b330f917a9f636c9511a
Jhonnatan Dussan <jhonnat.dussan@gmail.com>
2022-05-23 22:56:12 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { StudentModule } from './estudent/Student.module';
 import { SharedModule } from './shared/shared.module';
